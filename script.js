91d319f33026240e68cba66d6bb7085908ab73c7
Poonam Walale <155185680+poonamwalale07@users.noreply.github.com>
2024-02-11 00:59:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
+let inputBx = document.querySelector("#inputBx");
+let  list = document.querySelector('#list');
 
-buttons.forEach((item) => {
-  item.onclick = () => {
-    if (item.id == "clear") {
-      display.innerText = "";
-    } else if (item.id == "backspace") {
-      let string = display.innerText.toString();
-      display.innerText = string.substr(0, string.length - 1);
-    } else if (display.innerText != "" && item.id == "equal") {
-      display.innerText = eval(display.innerText);
-    } else if (display.innerText == "" && item.id == "equal") {
-      display.innerText = "Empty!";
-      setTimeout(() => (display.innerText = ""), 2000);
-    } else {
-      display.innerText += item.id;
+inputBx.addEventListener("keyup", function(event){
+    if (event.key == "Enter") {
+        addItem(this.value)
+        this.value = ""
     }
-  };
-});
+})
 
-const themeToggleBtn = document.querySelector(".theme-toggler");
-const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggler-icon");
-let isDark = true;
-themeToggleBtn.onclick = () => {
-  calculator.classList.toggle("dark");
-  themeToggleBtn.classList.toggle("active");
-  isDark = !isDark;
-};
+let addItem = (inputBx) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `${inputBx}<i></i>`;
+
+        listItem.addEventListener("click", function(){
+            this.classList.toggle('done');
+        })
+
+        listItem.querySelector("i").addEventListener("click",function(){
+            listItem.remove();
+        })
+
+        list.appendChild(listItem);
+}
